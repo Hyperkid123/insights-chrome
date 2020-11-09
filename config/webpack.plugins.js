60a9d9df507f10a5533b80a7dfe02fd0d7508c43
Martin Marosi <marvusm.mmi@gmail.com>
2020-11-09 13:10:21 +0100

    Update to webpack v5

--- config/webpack.plugins.js
+++ config/webpack.plugins.js
@@ -1,24 +1,22 @@
 const webpack = require('webpack');
-const LodashWebpackPlugin = require('lodash-webpack-plugin');
 const WriteFileWebpackPlugin = require('write-file-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
 
 const plugins = [
-    new CleanWebpackPlugin(),
-    new HardSourceWebpackPlugin(),
-    new WriteFileWebpackPlugin(),
-    new webpack.SourceMapDevToolPlugin({
-        test: /\.js/i,
-        exclude: /node_modules/i,
-        filename: `sourcemaps/[name].js.map`
-    }),
-    new LodashWebpackPlugin({
-        currying: true,
-        flattening: true,
-        placeholders: true,
-        paths: true
-    })
+  new ModuleFederationPlugin({
+    name: 'insights-chrome',
+    library: { type: 'var', name: 'insights-chrome' },
+    filename: 'chrome-remote.js',
+    shared: { react: { singleton: true, eager: true }, 'react-dom': { singleton: true, eager: true } },
+  }),
+  new CleanWebpackPlugin(),
+  new WriteFileWebpackPlugin(),
+  new webpack.SourceMapDevToolPlugin({
+    test: /\.js/i,
+    exclude: /node_modules/i,
+    filename: `sourcemaps/[name].js.map`,
+  }),
 ];
 
 module.exports = plugins;
